8cacdfcb6473d8732cc3b960d4f4bceec79868bb
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-06-26 13:33:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -358,7 +358,14 @@ ${refLink}`;
 
   const message = [baseInfo, promo, referrals].join('\n\n');
 
-  ctx.replyWithMarkdown(message, kb());
+// Отправляем сообщение с кнопкой подписки
+await ctx.replyWithMarkdown(message, {
+  ...kb(),
+  reply_markup: {
+    ...Markup.inlineKeyboard([
+      Markup.button.callback('✅ Я подписался', 'check_subscription')
+    ]).reply_markup
+  }
 });
 
 bot.hears(texts.upgrade, ctx => ctx.reply(texts.upgradeInfo));
