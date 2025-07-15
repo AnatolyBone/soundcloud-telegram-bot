1ac29ffd523f8fa809577c50a9f3b5ad1a39df1e
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-07-15 16:35:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import util from 'util';
 import NodeID3 from 'node-id3';
 import pgSessionFactory from 'connect-pg-simple';
 import pkg from 'pg';
-import { Parser } from '@json2csv/node';
+import json2csv from '@json2csv/node';
 import { supabase } from './db.js'; // указывай расширение!
 import expressLayouts from 'express-ejs-layouts';
 import https from 'https';
@@ -1017,17 +1017,8 @@ app.get('/export', requireAuth, async (req, res) => {
       return true;
     });
     
-    const fields = [
-      'id',
-      'username',
-      'first_name',
-      'total_downloads',
-      'premium_limit',
-      'created_at',
-      'last_active'
-    ];
-    
-    const parser = new Parser({ fields });
+    const fields = ['id', 'username', 'first_name', 'total_downloads', 'premium_limit', 'created_at', 'last_active'];
+    const parser = new json2csv.Parser({ fields });
     const csv = parser.parse(filteredUsers);
     
     res.header('Content-Type', 'text/csv');
