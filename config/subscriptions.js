8a418c8fd992ab0e997e45cdedaf8503163b10ff
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-07-23 07:59:28 +0300

    Create config/subscriptions.js

---
+++ config/subscriptions.js
@@ -0,0 +1,7 @@
+export const INVOICE_PAYLOAD_PREFIX = 'avandy-news';
+export const START_PARAMETER = 'payment-invoice';
+
+export const PLANS = {
+  monthly: { days: 30, limit: 50, price: 29900 },    // 299 руб в копейках
+  quarterly: { days: 90, limit: 120, price: 79900 }, // 799 руб в копейках
+};
