b347182774217e514594352e8614f94f6cc14d51
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-08-09 22:10:12 +0300

    Delete config/subscriptions.js

--- config/subscriptions.js
+++
@@ -1,7 +0,0 @@
-export const INVOICE_PAYLOAD_PREFIX = 'scloudmusicbot';
-export const START_PARAMETER = 'payment-invoice';
-
-export const PLANS = {
-  monthly: { days: 30, limit: 50, price: 29900 },    // 299 руб в копейках
-  quarterly: { days: 90, limit: 120, price: 79900 }, // 799 руб в копейках
-};
