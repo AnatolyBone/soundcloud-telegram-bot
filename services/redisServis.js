641096c791e62082ff9becbfccc5a3b6e2d4cdaf
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-08-09 22:16:15 +0300

    Create redisServis.js

---
+++ services/redisServis.js
@@ -0,0 +1,23 @@
+// services/redisService.js
+import { createClient } from 'redis';
+
+class RedisService {
+  constructor() {
+    this.client = null;
+  }
+
+  async connect() {
+    if (!this.client) {
+      this.client = createClient({ url: process.env.REDIS_URL });
+      await this.client.connect();
+    }
+    return this.client;
+  }
+
+  getClient() {
+    if (!this.client) throw new Error('Redis client is not initialized');
+    return this.client;
+  }
+}
+
+export default new RedisService();
