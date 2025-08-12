229f8979677ec9323a7934f1b066c7439cc75941
AnatolyBone <80332097+AnatolyBone@users.noreply.github.com>
2025-08-12 04:48:13 +0300

    Update botService.js

--- services/botService.js
+++ services/botService.js
@@ -1,5 +1,6 @@
 // services/botService.js
 import { setupTelegramHandlers } from '../src/botHandlers.js';
+import { downloadQueue } from './downloadManager.js';
 
 export default class BotService {
   constructor(bot) {
@@ -9,5 +10,10 @@ export default class BotService {
   setupTelegramBot() {
     console.log('🔌 Настройка обработчиков Telegram...');
     setupTelegramHandlers(this.bot);
+
+    // Мониторинг очереди теперь здесь
+    setInterval(() => {
+        console.log(`[Monitor] Очередь: ${downloadQueue.size} в ожидании, ${downloadQueue.active} в работе.`);
+    }, 60 * 1000);
   }
 }
